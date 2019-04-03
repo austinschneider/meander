5c3a512ea9e8d55679859b563c8e2ce145a8a576
Austin Schneider <hogenshpogen@gmail.com>
2019-04-02 22:54:57 -0500

    Basics for computing contours in other spaces require support for different geodesics.

--- meander/contour_compute.py
+++ meander/contour_compute.py
@@ -34,7 +34,7 @@ def connect_line_segments(lines):
         # delete the point that we've already handled
         del l_dict[p0]
 
-        #next
+        # next
         p0 = p1
 
         while True:
@@ -43,6 +43,7 @@ def connect_line_segments(lines):
                 polygons.append(polygon)
                 polygon = []
                 break
+
             # check if we've hit the end of a chain
             if len(l_dict[p0]) < 2:
                 # check if we've already done both ends
@@ -71,6 +72,101 @@ def connect_line_segments(lines):
             p0 = p1
 
     return polygons
+
+def _simplex_intersections(samples, levels, simplices):
+    """ Compute the simplex intersections of a iso-lines for a scalar field in a 2d space """
+
+    # to simplify the problem each point has an index
+    # and each line has an index
+
+    simplex_lines = np.zeros((len(levels),0)).tolist() # lines between the simplex edges that cross the levels
+    lines = [] # the lines that compose the simplex edges
+    li_to_si = dict() # maps line index -> simplex indices
+    l_to_li = dict() # maps line -> line index
+    for si, points in enumerate(simplices):
+        points = sorted(points)
+        # points and lines are ordered so lx does not contain px (px is opposite lx)
+        p0, p1, p2 = points
+        these_lines = [(p1,p2), (p0,p2), (p0,p1)]
+
+        # fill the maps
+        for l in these_lines:
+            if l not in l_to_li:
+                li_to_si[len(lines)] = []
+                l_to_li[l] = len(lines)
+                lines.append(l)
+            li = l_to_li[l]
+            li_to_si[li].append(si)
+
+        for level_i, level in enumerate(levels):
+            # which points are above / below the level
+            b = np.array([samples[p] >= level for p in points]).astype(bool)
+            b_sum = np.sum(b)
+
+            # skip triangles thar are all above or all below the level
+            # for triangles spanning the level, get the lines that cross the level
+            if b_sum == 1:
+                line_indices = np.array([l_to_li[l] for l in these_lines])[np.array([i for i in xrange(3) if i != np.argmax(b)])]
+            elif b_sum == 2:
+                line_indices = np.array([l_to_li[l] for l in these_lines])[np.array([i for i in xrange(3) if i != np.argmin(b)])]
+            else:
+                continue
+            simplex_lines[level_i].append(tuple(sorted(line_indices)))
+
+    simplex_intersections_by_level = []
+    for level_i, level in enumerate(levels):
+        connected_line_segments = connect_line_segments(simplex_lines[level_i])
+        simplex_intersections_by_level.append([np.array([lines[li] for li in index_contour]) for index_contour in connected_line_segments])
+
+    return simplex_intersections_by_level
+
+def _cartesian_geodesic(p0, p1, x):
+    # This function describes the geodesic between p0 and p1 in a 2d cartesian space
+    # p0 and p1 are assumed to be of the form (x0, y0) and (x1, y1)
+    assert(x >= 0.0 and x <= 1.0)
+    p = np.asarray(p0) * (1.0-x) + np.asarray(p1) * x
+    return p
+
+def _spherical_geodesic(p0, p1, x):
+    # This function describes the geodesic between p0 and p1 on a unit sphere
+    # p0 and p1 are assumed to be of the form (theta0, phi0) and (theta1, phi1)
+    # where theta and phi are the polar and azimuthal angles respectively measured in radians
+    assert(x >= 0.0 and x <= 1.0)
+    v0, v0 = [np.array((np.cos(phi)*np.sin(theta), np.sin(phi)*np.sin(theta), np.cos(theta))) for theta, phi in [p0, p1]]
+    c = np.sqrt(np.sum((v1-v0)**2))
+    theta = 2.0*np.arcsin(c/2.0)
+    theta0 = x*theta
+    theta1 = theta * (1.0 - x)
+    theta2 = (np.pi - theta) / 2.0
+    theta4 = np.pi - (theta1 + theta2)
+    c0 = np.sin(theta0) / np.sin(theta4)
+
+    v2 = v1 - v0
+    v2 = v2 / np.sqrt(np.sum(v2**2.0))
+    v3 = v0 + v2*c0
+    v3 = v3 / np.sqrt(np.sum(v3**2.0))
+
+    p = (np.arccos(v3[2]), np.arctan2(v3[1], v3[0]))
+    return p
+
+def _interpolate_simplex_intersections(sample_points, samples, levels, simplex_intersections_by_level, geodesic=_cartesian_geodesic):
+    contours_by_level = []
+    for level_i, level in enumerate(levels):
+        connected_line_segments = simplex_intersections_by_level[level_i]
+        lines_in_contours = np.unique([line for index_contour in connected_line_segments for line in index_contour], axis=0)
+
+        line_ps = dict()
+
+        for line in lines_in_contours:
+            p0, p1 = line
+            (p0, y0), (p1, y1) = sorted([(p0, samples[p0]), (p1, samples[p1])], key=lambda x: x[1])
+            x = (level-y0) / (y1-y0)
+            p = geodesic(sample_points[p0], sample_points[p1], x)
+            line_ps[li] = p
+        line_ps = np.array(line_ps)
+
+        contours_by_level.append([np.array([line_ps[li] for li in index_contour]) for index_contour in connect_line_segments(simplex_lines)])
+    return contours_by_level
 
 def compute_contours(sample_points, samples, level):
     """ Compute contours (iso-lines of a scalar field) in a 2d cartesian space """
